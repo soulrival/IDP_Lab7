8aa11c1d669b35cca4e3fbec6aa3b90811055087
Artme Zhdanov <zhdanov.artyom231003@gmail.com>
2023-06-10 17:38:34 +0300

    merge xml json commit

--- WeatherService.cpp
+++ WeatherService.cpp
@@ -1,8 +1,15 @@
 ﻿#include "XmlService.h"
+#include "JsonService.h"
 
 int main()
 {
-	XmlService js;
-	Weather w = js.getWeather("weather.xml");
-	cout << w;
+    setlocale(LC_ALL, "RU");
+    cout << "Json-файл:" << endl;
+    JsonService js;
+    Weather w1 = js.getWeather("weather.json");
+    cout << w1 << endl << endl;
+    cout << "Xml-файл:" << endl;
+    XmlService xs;
+    Weather w2 = xs.getWeather("weather.xml");
+    cout << w2;
 }
